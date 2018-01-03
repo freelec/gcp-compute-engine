6670fc436959a494a410361a0043013d71185be3
freelec <35051545+freelec@users.noreply.github.com>
2018-01-03 17:51:13 +0900

    Update app.py

--- app_v1/app.py
+++ app_v1/app.py
@@ -33,8 +33,8 @@ def linesep_to_br_filter(s):
     return escape(s).replace('\n', Markup('<br>'))
 
 class MessageForm(Form):
-    input_name = StringField(u'お名前', [validators.Length(min=1, max=16)])
-    input_message = TextAreaField(u'メッセージ',
+    input_name = StringField(u'이름', [validators.Length(min=1, max=16)])
+    input_message = TextAreaField(u'메시지',
                                   [validators.Length(min=1, max=1024)])
 
 @app.route('/')
